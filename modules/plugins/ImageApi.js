c835c3c038e254731f24d96de1196fdcf6e4fd1f
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-01 05:17:38 -0500

    image api helper.

---
+++ modules/plugins/ImageApi.js
@@ -0,0 +1,73 @@
+let supportsWebP = false
+export default async function (ctx, inject) {
+
+
+  await (async (inject) => {
+    if(await supportsWebp()) // check if supports webp
+      supportsWebP = true
+  })()
+
+  inject('CBDImage', {
+    getFromServer: getFromServer,
+    getFromClient: getFromClient,
+    get:get
+  })
+
+  //============================================================
+  //
+  //============================================================
+  function get(name, width, height) {
+    if(ctx.isServer) return getFromServer(name, width, height)
+    if(ctx.isClient) return getFromClient(name, width, height)
+  }
+
+  //============================================================
+  //
+  //============================================================
+  function getFromServer(name, width, height) {
+
+    if(!ctx.isServer) throw new Error('GetImageServer being called from client')
+    if(!name || !width) throw new Error('GetImageServer Lazy load image request must have a name and width.')
+
+    if(!height) height = width
+
+    return `http://cbddocumentsimages-imagebucket-emyy2umszkrb.s3-website-us-east-1.amazonaws.com/${width}x${height}/${name}`
+  }
+
+  //============================================================
+  //
+  //============================================================
+  function getFromClient(name, width, height) {
+    if(!ctx.isClient) throw new Error('GetImageClient being called from client')
+    if(!name || !width) throw new Error('GetImageClient Lazy load image request must have a name and width.')
+
+    if(!height) height = width
+
+    if(supportsWebP  && !~name.indexOf('.webp')){
+      let fileExt = getFileExt()
+      let pattern = `/${fileExt}/gi`
+      name = name.replace(pattern, '.webp');
+      return `http://cbddocumentsimages-imagebucket-emyy2umszkrb.s3-website-us-east-1.amazonaws.com/${width}x${height}/${name}`
+    }
+
+    return `http://cbddocumentsimages-imagebucket-emyy2umszkrb.s3-website-us-east-1.amazonaws.com/${width}x${height}/${name}`
+  }
+
+  //============================================================
+  //
+  //============================================================
+  async function supportsWebp() {
+    if (ctx.isServer || !self.createImageBitmap) return false;
+
+    const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+    const blob = await fetch(webpData).then(r => r.blob());
+    return createImageBitmap(blob).then(() => true, () => false);
+  }
+}
+
+
+
+function getFileExt(filename) {
+  var r = /.+\.(.+)$/.exec(filename);
+  return r ? r[1] : null;
+}
