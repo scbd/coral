663c58ef57ef75d7ff71ae733c67b3494beab367
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-01 05:06:58 -0500

    twitter grid (+5mb on initial load)

---
+++ components/home/TwitterGrid.vue
@@ -0,0 +1,30 @@
+<template>
+  <section ref="tgrid">
+    <a class="twitter-grid" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">
+      National Park Tweets
+    </a>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'TwitterGrid',
+    asyncData ({app,isClient}) {
+      return {
+        isClient: isClient
+      }
+    },
+    mounted() {
+        const script = document.createElement('script')
+        script.setAttribute('type', 'text/javascript')
+        script.setAttribute('charset', 'utf-8')
+        script.setAttribute('async', 'async')
+        script.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+        this.$refs.tgrid.appendChild(script)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
