bf6c24997357102b294da805aeb69720b54cb83b
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-04 19:40:43 -0500

    remove animation

--- components/scbd/GlobalBar/locale/Locale.vue
+++ components/scbd/GlobalBar/locale/Locale.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
     <div class="navbar-item has-dropdown is-hoverable" >
-      <a class="navbar-link is-uppercase cbd-size" v-on:click="showMobile()" v-if="!mobileOptionsOnly">
-        <span v-show="!showMobileFlag"><i class="fontello icon-language is-size-6" ></i></span>
-        <span class="is-hidden-desktop" v-show="showMobileFlag"><i class="fontello icon-cancel is-size-5" ></i></span>
+      <a class="navbar-link is-uppercase cbd-size is-paddingless" v-on:click="showMobile()" v-if="!mobileOptionsOnly">
+        <span v-show="!showMobileFlag"><i class="fontello icon-language is-size-5" ></i></span>
+        <span class="is-hidden-desktop is-pulled-right" v-show="showMobileFlag"><i class="fontello icon-cancel is-size-5" ></i></span>
       </a>
       <options class="is-hidden-desktop" v-show="showMobileFlag && mobileOptionsOnly" />
       <options class="is-hidden-touch" />
@@ -43,20 +43,9 @@ export default {
 </script>
 <style scoped>
 .cbd-size{
-  font-size: 14px;
+  font-size: 1em;
 }
-
-/* Enter and leave animations can use different */
-/* durations and timing functions.              */
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translatey(-5px);
-  opacity: 0;
+.navbar-link:after{
+  border: unset;
 }
 </style>
