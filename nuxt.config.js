934e9cd11bb7174961f78bdc1369fd150e2ea8dc
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-05 12:23:31 -0500

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {name:'apple-mobile-web-app-capable',content:'yes'}
+      { name:'apple-mobile-web-app-capable',content:'yes'}
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'assets/images/favicon.ico' }
