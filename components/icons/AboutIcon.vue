affb076fb592e69b6892fbd8b7f62e513e311f54
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-04 19:40:17 -0500

    icons as component exp.

---
+++ components/icons/AboutIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :style="{fill:color}"  viewBox="0 0 164.93 134">
+    <title>About Coral Reefs Icon</title>
+    <path d="M10.31,106.51H154.62a3.44,3.44,0,0,0,3.44-3.44h0V13.74A13.76,13.76,0,0,0,144.31,0H20.62A13.76,13.76,0,0,0,6.87,13.74v89.33a3.44,3.44,0,0,0,3.42,3.44ZM20.62,13.74H144.31V89.33H20.62Z" transform="translate(0)"/>
+    <path  d="M161.49,113.38H99.64a3.44,3.44,0,0,0-3.44,3.42v3.46H68.72v-3.44a3.44,3.44,0,0,0-3.44-3.44H3.44A3.44,3.44,0,0,0,0,116.8v6.89A10.33,10.33,0,0,0,10.31,134H154.62a10.33,10.33,0,0,0,10.31-10.31v-6.87A3.44,3.44,0,0,0,161.49,113.38Z" transform="translate(0)"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'AboutIcon',
+  props: {
+      color: {
+          type: String,
+          default: '#000000'
+      }
+  }
+}
+</script>
