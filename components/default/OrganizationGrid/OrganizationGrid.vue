0f2948db8ec7e4d6122de1b3ad14b9707fdb6be0
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-04 19:38:43 -0500

    organization display component

---
+++ components/default/OrganizationGrid/OrganizationGrid.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrganizationGrid'
+}
+</script>
+<style scoped>
+  .grid{
+    display: grid;
+    grid-template-columns: 1fr ;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    .grid{
+      display: grid;
+      grid-template-columns: 1fr 1fr ;
+      grid-gap: 1em;
+    }
+  }
+
+  @media (min-width:1024px){
+    .grid{
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-gap: 1em;
+    }
+  }
+
+</style>
---
+++ components/default/OrganizationGrid/OrganizationGridItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card grid-org">
+    <a :href="url" target="_blank" rel="noopener">
+      <div class="card-image is-text-centered" >
+        <figure class="grid-org-figure has-text-centered" >
+          <img class="figure-img" :src="image" :alt="title" :title="title">
+        </figure>
+      </div>
+      <div class="card-content grid-org-content">
+        <div class="content">
+          {{title}}
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'OrganizationGridItem',
+  props: {
+      title: {
+        type: String,
+        default: 'text'
+      },
+      image: {
+          type: String,
+          default: 'text'
+      },
+      url: {
+          type: String,
+          default: 'text'
+      },
+      backImage: {
+          type: String
+      }
+  }
+}
+</script>
+<style scoped>
+  .content{
+    font-weight: 500;
+        height:100%;
+  }
+  .card{
+    border: 1px solid #0086b7;
+  }
+  .card-image{
+    margin: 5px 5px 5px 5px;
+  }
+  .grid-org-figure{
+
+    line-height: 100px;
+    max-height: 100px;
+  }
+  .figure-img{
+    vertical-align: middle;
+    max-height: 100px;
+    max-width: 90%;
+  }
+  .grid-org-content{
+    background-color: #0086b7;
+    color: #ffffff;
+  }
+</style>
