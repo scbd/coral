a38a2e2e4d1c8826b60d3980c249d27a82b100be
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-08 12:14:35 -0500

    meta updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,8 +15,9 @@ module.exports = {
     titleTemplate: '%s | UN Biodiversity',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: '#00405c' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'assets/images/favicon.ico' }
