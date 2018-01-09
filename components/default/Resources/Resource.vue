0a45afe5bfd4639c6bd951ee6d3396a8644ef52e
Randy Houlahan <randy.houlahan@cbd.int>
2018-01-09 09:41:14 -0500

    resources size

--- components/default/Resources/Resource.vue
+++ components/default/Resources/Resource.vue
@@ -46,28 +46,7 @@ export default {
       return tcText + ' ...'
     }
   }
-  // filters:{
-  //   //from https://github.com/imcvampire/vue-truncate-filter
-  //   truncate: function (text, length, clamp){
-  //     clamp = clamp || '...';
-  //     length = length || 30;
-  //
-  //     if (text.length <= length) return text;
-  //
-  //     var tcText = text.slice(0, length - clamp.length);
-  //     var last = tcText.length - 1;
-  //
-  //
-  //     while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0]) last -= 1;
-  //
-  //     // Fix for case when text dont have any `space`
-  //     last = last || length - clamp.length;
-  //
-  //     tcText =  tcText.slice(0, last);
-  //
-  //     return tcText + clamp;
-  //   }
-  //}
+
 }
 
 </script>
@@ -78,6 +57,7 @@ export default {
     color: #ffffff;
     border-radius: 20px 20px 20px 20px;
     position:relative;
+    min-height: 200px;
   }
   .card a{
     color:#ffffff;
