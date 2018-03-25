01224f89585ba73dc4c2438144ed7ed80f9d91ff
Randy Houlahan <randy.houlahan@cbd.int>
2018-03-25 17:46:37 -0400

    new coral text section

---
+++ components/CoralSectionTwo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="holder-two"  v-lazy:background-image="$CBDImage.get('blue-div-with-pink-embellishments.jpg')">
+    <div class="text-holder-two">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CoralSectionTwo'
+  }
+</script>
+
+<style scopped>
+  .holder-two{
+    display:flex;
+    flex-direction:row;
+    justify-content: center;
+    align-items: center;
+    padding: 3em 3em 3em 3em;
+    background-size: cover;
+    background-position: center top;
+    background-repeat: no-repeat;
+        margin-bottom: 1.5em;
+  }
+  .text-holder-two{
+    max-width:400px;
+    min-width:200px;
+    color:white;
+    text-align: justify;
+  }
+</style>
