e981a1bc8862b578d957dc65614af4d2fcbbf360
Randy Houlahan <randy.houlahan@cbd.int>
2018-03-29 11:38:22 -0400

    update resources

--- pages/_lang/resources.vue
+++ pages/_lang/resources.vue
@@ -10,15 +10,36 @@
           <div class="debug">
             <div class="columns">
               <div class="column">
-                <div class="search-title">Filter by Keyword</div>
-                <div>body</div>
+                <div class="search-title">{{$t('filterByKeyword')}}</div>
+                <div>
+                   <div class="field">
+                    <p class="control has-icons-left has-icons-right">
+                      <input class="input"  type="text" :placeholder="$t('keyWordPhrase')">
+
+                      <span class="icon is-small is-right">
+                        <i class="fontello icon-search" ></i>
+                      </span>
+                    </p>
+                  </div>
+                </div>
               </div>
               <div class="column">
-                <div class="search-title">title</div>
-                <div>body</div>
+                <div class="search-title">{{$t('filterByType')}}</div>
+                <div>
+                  <div>
+                    <label class="checkbox debug">
+                      <input type="checkbox">
+                      {{$t('cBDMaterials')}}
+                    </label>
+                  </div>
+                  <label class="checkbox">
+                    <input type="checkbox">
+                    {{$t('education')}}
+                  </label>
+                </div>
               </div>
               <div class="column">
-                <div class="search-title">title</div>
+                <div class="search-title">{{$t('filterByDate')}}</div>
                 <div>body</div>
               </div>
             </div>
@@ -64,8 +85,14 @@
 <style>
   .search{
     border: 6px solid #ff3e94;
+    margin: .5em 1em 1em 1em;
+    padding: .5em .5em .5em .5em;
   }
   .search-title{
     font-weight: 900;
   }
+input[type=checkbox]{
+    vertical-align: middle !important;
+    margin-bottom: .3em;
+}
 </style>
--- store/modules/schemas/resource.js
+++ store/modules/schemas/resource.js
@@ -2,15 +2,35 @@ import Vue    from 'vue'
 import api    from '~/modules/api'
 
 var state = {
-  docs: {en:[],fr:[],ru:[],ar:[],zh:[],es:[]}
+  docs: {en:[],fr:[],ru:[],ar:[],zh:[],es:[]},
+  edMaterials:[
+    '0CA88602-7281-0EBC-CF92-B6E085145173',
+    '11C1EC87-ACCB-8B8A-9AEE-F1D63510FDB5',
+    '422519CE-2FE5-E5CF-DD7D-66BC8ADB9073',
+    '0C5AA1D9-EEE5-05A3-1096-963A7A37D4F1',
+    'B9130D14-CD29-2BFA-CC53-DCCFE7937758',
+    'A8823BA2-2FD5-CF6D-EAD9-2E152BFEC9B9'
+  ],
+  scdbMaterials:['226C2B5C-4A21-B065-3714-997A10D98B15',
+                  'A95C31F7-1897-5EC7-FC04-669D05FC3855',
+                  '4A04D1AE-A394-0B6C-56D0-84A1308D7F37',
+                  '5F92F728-5197-C20B-C888-5C48966BA9F9',
+                  'AA555853-BE32-9AF7-D007-99FA1C77A107',
+                  '3F20A40B-2448-A6FE-C975-02EE5844BBE3',
+                  '5540F0D7-322C-873F-9979-9B9A75135E1E',
+                  '61DED9DE-1982-6173-C234-85862F6E3D3B',
+                  '24971E7F-DA65-1A0A-9E33-054E1003B987',
+                  '2FF2CA9B-F994-A1C0-298F-3BC05A878700']
 }
 
 const actions = {
-  get: getResource
+  get: getResource,
+  getEd:getEdMaterials
 }
 
 const getters = {
   getByIdentifier: getByIdentifier
+  // getFirstYear: getFirstYear
 }
 const mutations = {
   set: setMutation
@@ -87,7 +107,71 @@ async function getResource({state,dispatch,commit,rootState},data){
         console.log(e)
     }
 }
+function edQuery(edIds){
+    return '('+edIds.join(' OR ')+')'
+}
+//============================================================
+//
+//============================================================
+async function getEdMaterials({state,dispatch,commit,rootState},data){
 
+    let response='';
+    let locale = rootState.locale.locale
+    console.log(edQuery(state.edMaterials))
+    let queryParameters = {
+        'q': 'realm_ss:chm AND (schema_s:resource)',
+        'sort': 'createdDate_dt desc',
+        'fl': `identifier_s,title_${locale.toUpperCase()}_t,summary_${locale.toUpperCase()}_t,description_${locale.toUpperCase()}_t,url_ss,createdDate_dt,startDate_dt`,
+        'wt': 'json',
+        'rows':10000
+    }
+
+    try {
+
+        response = await api.ax.get('https://api.cbd.int/api/v2013/index/select', {'params': queryParameters})
+
+        switch(response.status){
+            case 200:
+                let payload = {}
+                payload.locale = locale
+                payload.docs = response.data.response.docs
+                commit('set',payload)
+            break;
+            case 401:
+                commit('feedback/warn',{
+                    title:'Not Authorized',
+                    description:'Your email and/or password are incorrect.'
+                },{ root: true })
+            break;
+            case 404:
+                commit('feedback/danger',{
+                    title:'Not Found',
+                    description:'The authentication service is not found'
+                },{ root: true })
+            break;
+            case 400:
+                commit('feedback/danger',{
+                    title:'Bad Request',
+                    description:'The request to the authentication service is incorrect.'
+                },{ root: true })
+            break;
+            default:
+                commit('feedback/danger',{
+                    title:'Unkown Error Occured default',
+                    description:'An unkown error occured.'
+                },{ root: true })
+            break
+        }
+
+    } catch (e){
+        commit('feedback/danger',{
+            title:'Unkown Error Occured in catch',
+            description:'An unkown error occured.',
+            e:e
+        },{ root: true })
+        console.log(e)
+    }
+}
 //============================================================
 //
 //============================================================
@@ -108,7 +192,12 @@ function getByIdentifier (state) {
   return (identifier) => { return state.docs.find(doc => doc.identifier_s === identifier) }
 }
 
-
+//============================================================
+//
+//============================================================
+function getFirstYear (state) {
+  return (identifier) => { return state.docs.find(doc => doc.identifier_s === identifier) }
+}
 //============================================================
 //
 //============================================================
