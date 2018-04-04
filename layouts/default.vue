9bd7b6562ed51579bd2fb7371aa1417eebcf3846
Randy Houlahan <randy.houlahan@cbd.int>
2018-04-04 09:17:43 -0400

    fix page title/descriptions

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,13 +44,11 @@
         meta: [
           // We use $route.path since we don't use query parameters
           { hid:'lang', name: 'lang', content: this.$i18n.locale },
-          { hid:'og:title', name: 'og:title', content: this.$i18n.t('title') },
-          { hid:'og:description', name: 'og:description', content: this.$i18n.t('description') },
+
           { hid:'og:image', name: 'og:image', content: 'http://cbddocumentsimages-imagebucket-emyy2umszkrb.s3-website-us-east-1.amazonaws.com/1200x630/red-coral-blue-back.jpg' },
           { hid:'twitter:card', name: 'twitter:card', content: 'summary' },
           { hid:'twitter:site', name: 'twitter:site', content: '@CBDNews' },
-          { hid:'twitter:title', name: 'twitter:title', content: this.$i18n.t('title') },
-          { hid:'twitter:description', name: 'twitter:description', content: this.$i18n.t('description') },
+
           { hid:'twitter:image', name: 'twitter:image', content: 'http://cbddocumentsimages-imagebucket-emyy2umszkrb.s3-website-us-east-1.amazonaws.com/1200x675/red-coral-blue-back.jpg' },
           { hid:'twitter:image:alt', name: 'twitter:image:alt', content: 'CBD Coral Reefs Portal' }
         ],
--- modules/mixins/page.js
+++ modules/mixins/page.js
@@ -13,7 +13,11 @@ export default {
     return {
       title: this.$i18n.t('title'),
       meta: [
-        { hid: 'description', name: 'description', content: this.$i18n.t('description') }
+        { hid: 'description', name: 'description', content: this.$i18n.t('description') },
+        { hid:'twitter:title', name: 'twitter:title', content: this.$i18n.t('title') },
+        { hid:'twitter:description', name: 'twitter:description', content: this.$i18n.t('description') },
+        { hid:'og:title', name: 'og:title', content: this.$i18n.t('title') },
+        { hid:'og:description', name: 'og:description', content: this.$i18n.t('description') }
       ]
     }
   }
