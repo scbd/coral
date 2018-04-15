773cbadecfb692a485ea4c10cf75079a4825c628
Randy Houlahan <randy.houlahan@cbd.int>
2018-04-15 16:29:42 -0400

    ass env vars in ci

--- compile.sh
+++ compile.sh
@@ -1,19 +1,19 @@
 if [ "${CIRCLE_BRANCH}" == "dev" ]
 then
-  export baseUrl=${DevBaseUrl}
-  export apiUrl=${DevApiUrl}
+  export base=${DevBaseUrl}
+  export api=${DevApiUrl}
 elif [ "${CIRCLE_BRANCH}" == "stg" ]
 then
-  export baseUrl=${StgBaseUrl}
-  export apiUrl=${StgApiUrl}
+  export base=${StgBaseUrl}
+  export api=${StgApiUrl}
 elif [ "${CIRCLE_BRANCH}" == "master" ]
 then
-  export baseUrl=${baseUrl}
-  export apiUrl=${apiUrl}
-  npm run build
+  export base=${baseUrl}
+  export api=${apiUrl}
 fi
 
 echo "Sucessfull build for environment: ${CIRCLE_BRANCH}"
 echo "api: ${apiUrl}"
 echo "url: ${baseUrl}"
+docker build -t output --build-arg COMMIT=$CIRCLE_SHA1 --build-arg baseUrl=${base} --build-arg apiUrl=${api} .
 exit 0
